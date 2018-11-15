39d4116d72f4b58b14b5ec69fe270213e3384808
zhangwenbin <zhangwb@lhcis.com>
2018-11-15 20:10:33 +0800

    'scr'

--- src/UI/scroll/scroll.vue
+++ src/UI/scroll/scroll.vue
@@ -26,22 +26,21 @@
           if (/isScroll/.test(key.className)  && self.isEle) {
             self.isEle = false;
             self.getBind(key);
+            key.stopPropagation();
           }
         })
       },
       getBind(ele) {
-        ele.onmouseenter = this.getMouseEnter(ele);
-        ele.onmouseleave = this.getMouseLeave(ele);
+        ele.onmouseenter = this.getMouseEnter;
+        ele.onmouseleave = this.getMouseLeave;
+        return false;
       },
-      getMouseEnter(el) {
+      getMouseEnter(e) {
+        let el = e.target || e.srcElement;
         let version = window.navigator.userAgent;
-        this.pv = el;
-        this.tp1 = el.offsetHeight;
-        this.tp2 = el.parentNode.offsetHeight;
-        this.tp = this.tp2 * this.tp2 / this.tp1;
-        this.y = this.tp2 - this.tp;
-        this.tp3 = this.tp1 - this.tp2;
-        if (this.tp1 > this.tp2) {
+        let tp1 = el.offsetHeight;
+        let tp2 = el.parentNode.offsetHeight;
+        if (tp1 > tp2) {
           this.isShow = true;
         } else {
           this.isShow = false;
@@ -51,34 +50,47 @@
         } else {
           el.onmousewheel = this.getScroll;
         }
+        return false;
       },
-      getMouseLeave(el) {
+      getMouseLeave(e) {
+        let el = e.target || e.srcElement;
         let version = window.navigator.userAgent;
         this.isShow = false;
-        this.pv = null;
         if (/Firefox/.test(version)) {
           el.addEventListener('DOMMouseScroll', null, false);
         } else {
           el.onmousewheel = null;
         }
+        return false;
       },
       getScroll(e) {
-        let num = 120, top = 0, tp = 0;
+        let lists = this.$el.parentNode.childNodes;
+        console.log(lists)
+        lists.forEach((key) => {
+          if (/isScroll/.test(key.className)) {
+            let num = 120, top = 0, tp = 0;
+            let box = this.$el.offsetHeight;
+            console.log(box)
+          }
+        });
+return false;
         this.nm = num * this.y / this.tp3;
         if (this.nm >= this.y) {
           this.nm = this.y;
+        }
+        console.log(this.tp3)
+        if (this.tp3 <= 120) {
+          num = this.tp3;
         }
         if (e.wheelDelta) {
           if (e.wheelDelta > 0) { //向上
             this.nm = 0 - this.nm;
             num = 0 - num;
             top = top + this.nm;
-            tp = tp + num;
-            if (top < 0) { top = 0;};
-            if (tp < 0) { tp = 0;};
+
           } else {
             top = top + this.nm;
-            tp = tp - 120;
+
           }
 
         } else if (e.detail) {
@@ -86,23 +98,18 @@
             this.nm = 0 - this.nm;
             num = 0 - num;
             top = top + this.nm;
-            tp = tp + num;
-            if (top < 0) { top = 0;};
-            if (tp < 0) { tp = 0;};
+
           } else {
             top = top + this.nm;
-            tp = tp - 120;
+
           }
-        }
-        console.log(tp)
-        if (tp <= -this.tp3) {
-          tp = 0 - this.tp3;
         }
         if (this.top >= 0 && this.top <= this.y) {
           this.top = top;
         }
-        if (tp >= 0 && tp >= -this.tp3) {
-          this.pv.style.top = tp + 'px';
+
+        if (tp <= 0 && tp >= -this.tp3) {
+          //el.style.top = tp + 'px';
         }
       }
     }

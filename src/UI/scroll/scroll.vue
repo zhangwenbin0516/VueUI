ea0c9dbc158c6d206da517257da2eabb7852d976
zhangwenbin <zhangwb@lhcis.com>
2018-11-16 17:19:10 +0800

    'scroll'

--- src/UI/scroll/scroll.vue
+++ src/UI/scroll/scroll.vue
@@ -24,93 +24,45 @@
         this.isEle = true;
         this.lists.forEach((key) => {
           if (/isScroll/.test(key.className)  && self.isEle) {
+            this.$ele = key;
             self.isEle = false;
             self.getBind(key);
-            key.stopPropagation();
           }
         })
       },
       getBind(ele) {
-        ele.onmouseenter = this.getMouseEnter;
-        ele.onmouseleave = this.getMouseLeave;
-        return false;
+        ele.onmouseover = this.getMouseEnter;
+        ele.onmouseout = this.getMouseLeave;
       },
       getMouseEnter(e) {
         let el = e.target || e.srcElement;
-        let version = window.navigator.userAgent;
-        let tp1 = el.offsetHeight;
-        let tp2 = el.parentNode.offsetHeight;
-        if (tp1 > tp2) {
-          this.isShow = true;
-        } else {
-          this.isShow = false;
+        if (/isScroll/.test(this.$ele.className)) {
+          let version = window.navigator.userAgent;
+          let tp1 = this.$ele.offsetHeight;
+          let tp2 = this.$ele.parentNode.offsetHeight;
+          if (tp1 > tp2) {
+            this.isShow = true;
+          } else {
+            this.isShow = false;
+          }
+          if (/Firefox/.test(version)) {
+            el.addEventListener('DOMMouseScroll', this.getScroll, false);
+          } else {
+            el.onmousewheel = this.getScroll;
+          }
         }
-        if (/Firefox/.test(version)) {
-          el.addEventListener('DOMMouseScroll', this.getScroll, false);
-        } else {
-          el.onmousewheel = this.getScroll;
-        }
-        return false;
       },
       getMouseLeave(e) {
         let el = e.target || e.srcElement;
         let version = window.navigator.userAgent;
-        this.isShow = false;
         if (/Firefox/.test(version)) {
-          el.addEventListener('DOMMouseScroll', null, false);
+          this.$ele.ddEventListener('DOMMouseScroll', null, false);
         } else {
-          el.onmousewheel = null;
+          this.$ele.onmousewheel = null;
         }
-        return false;
       },
       getScroll(e) {
-        let lists = this.$el.parentNode.childNodes;
-        console.log(lists)
-        lists.forEach((key) => {
-          if (/isScroll/.test(key.className)) {
-            let num = 120, top = 0, tp = 0;
-            let box = this.$el.offsetHeight;
-            console.log(box)
-          }
-        });
-return false;
-        this.nm = num * this.y / this.tp3;
-        if (this.nm >= this.y) {
-          this.nm = this.y;
-        }
-        console.log(this.tp3)
-        if (this.tp3 <= 120) {
-          num = this.tp3;
-        }
-        if (e.wheelDelta) {
-          if (e.wheelDelta > 0) { //向上
-            this.nm = 0 - this.nm;
-            num = 0 - num;
-            top = top + this.nm;
-
-          } else {
-            top = top + this.nm;
-
-          }
-
-        } else if (e.detail) {
-          if (e.detail < 0) { //向上
-            this.nm = 0 - this.nm;
-            num = 0 - num;
-            top = top + this.nm;
-
-          } else {
-            top = top + this.nm;
-
-          }
-        }
-        if (this.top >= 0 && this.top <= this.y) {
-          this.top = top;
-        }
-
-        if (tp <= 0 && tp >= -this.tp3) {
-          //el.style.top = tp + 'px';
-        }
+        console.log(e)
       }
     }
   }
